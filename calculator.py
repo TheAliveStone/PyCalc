ff40014e719a320748b2eb66d14e23dfe2dfaade
IronPresident <livingstoneobaje22@gmail.com>
2025-07-22 12:32:42 +0100

    Create the calculator

--- calculator.py
+++ calculator.py
@@ -1,1 +1,22 @@
+import math
 
+operator = input("Enter an operator (+, -, *, /, ^, sqrt): ")
+if operator == "sqrt":
+    num1 = int(input("Enter your first number: "))
+else:
+    num1 = int(input("Enter your first number: "))
+    num2 = int(input("Enter your twice number: "))
+ans = 0
+
+if operator == "+":
+    ans = num1 + num2
+elif operator == "-":
+    ans = num2 - num1
+elif operator == "*":
+    ans = num1 * num2
+elif operator == "/":
+    ans = num1 / num2
+elif operator == "^":
+    ans = num1 ^ num2
+elif operator == "sqrt":
+    ans = math.sqrt(num1)
