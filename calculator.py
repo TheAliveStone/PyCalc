e28a0e825c9b9099ce66e3c9a428fb0dc43fb60b
IronPresident <livingstoneobaje22@gmail.com>
2025-07-22 23:16:35 +0100

    Refactor calculator logic to include error handling and improve user input flow

--- calculator.py
+++ calculator.py
@@ -1,23 +1,27 @@
 import math
 
-operator = input("Enter an operator (+, -, *, /, ^, sqrt): ")
-if operator == "sqrt":
-    num1 = float(input("Enter your first number: "))
-else:
-    num1 = float(input("Enter your first number: "))
-    num2 = float(input("Enter your twice number: "))
-ans = 0
-
-if operator == "+":
-    ans = num1 + num2
-elif operator == "-":
-    ans = num2 - num1
-elif operator == "*":
-    ans = num1 * num2
-elif operator == "/":
-    ans = num1 / num2
-elif operator == "^":
-    ans = num1 ^ num2
-elif operator == "sqrt":
-    ans = math.sqrt(num1)
-print(f"The answer is: " + ans)
+while True:
+    operator = input("Enter an operator (+, -, *, /, ^, sqrt): ")
+    try:
+        if operator == "sqrt":
+            num1 = float(input("Enter your number: "))
+            ans = math.sqrt(num1)
+        else:
+            num1 = float(input("Enter your first number: "))
+            num2 = float(input("Enter your second number: "))
+            if operator == "+":
+                ans = num1 + num2
+            elif operator == "-":
+                ans = num1 - num2
+            elif operator == "*":
+                ans = num1 * num2
+            elif operator == "/":
+                ans = num1 / num2
+            elif operator == "^":
+                ans = num1 ** num2
+            else:
+                print("Your operator is invalid. Please try again")
+                continue
+        print(f"The answer is: {ans}")
+    except Exception as e:
+        print(f"Error: {e}") 
